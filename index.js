b93090b52667cf4d5faf796aa44ba55ca8a36564
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 19:51:34 -0500

    right dice

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
 //Test it out in the console to make sure it works as expected.
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
 let leftDice = document.querySelector(".img1")
 
+let rightDice = document.querySelector(".img2")
+
 leftDice.setAttribute("src", `images/dice${randomNumber1}.png`)
+
+rightDice.setAttribute("src", `images/dice${randomNumber2}.png`)
+
