f59eb9f2a7d56604dfc3f742726d90fcc94f915d
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 22:20:38 -0500

    comments re facotr

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 
-let randomNumber1 = Math.floor(Math.random() * 6) + 1;
-let randomNumber2 = Math.floor(Math.random() * 6) + 1;
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;  //random number for dice 1
+let randomNumber2 = Math.floor(Math.random() * 6) + 1;  //random number for dice 2
 
-let leftDice = document.querySelector(".img1")
+let leftDice = document.querySelector(".img1")  //select the left dice
 
-let rightDice = document.querySelector(".img2")
+let rightDice = document.querySelector(".img2") //select the right dice
 
-leftDice.setAttribute("src", `images/dice${randomNumber1}.png`)
+leftDice.setAttribute("src", `images/dice${randomNumber1}.png`)     //change the left dice image
 
-rightDice.setAttribute("src", `images/dice${randomNumber2}.png`)
+rightDice.setAttribute("src", `images/dice${randomNumber2}.png`)    //change right dice image
 
 
 
-let winner = document.querySelector("h1")
+let winner = document.querySelector("h1")       //select the heading text
 
 //ternary to change heading 
 winner.innerText = randomNumber1 === randomNumber2 ? "Draw" : randomNumber1 > randomNumber2 ? "Player 1 Wins!" : "Player 2 Wins!"
