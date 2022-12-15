2fa4a79c53d92ef9bbbb1647061d60cbd99ba515
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 19:49:34 -0500

    random left dice

--- index.js
+++ index.js
@@ -2,3 +2,8 @@
 //the value of this variable to a random number between 1 and 6. 
 //Test it out in the console to make sure it works as expected.
 
+let randomNumber1 = Math.floor(Math.random() * 6) + 1;
+
+let leftDice = document.querySelector(".img1")
+
+leftDice.setAttribute("src", `images/dice${randomNumber1}.png`)
