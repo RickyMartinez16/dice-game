6a91c6acfe6f93749c0b4d35409a1abe8b62be22
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 22:19:17 -0500

    re factor

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-// Inside index.js, create a new variable called randomNumber1 then set 
-//the value of this variable to a random number between 1 and 6. 
-//Test it out in the console to make sure it works as expected.
 
 let randomNumber1 = Math.floor(Math.random() * 6) + 1;
 let randomNumber2 = Math.floor(Math.random() * 6) + 1;
