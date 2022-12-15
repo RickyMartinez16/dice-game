6f52f74d888023ba943a1c9538229a58b4c2083e
Ricky Martinez <RMartinezW16@gmail.com>
2022-12-14 20:02:58 -0500

    change heading

--- index.js
+++ index.js
@@ -13,3 +13,20 @@ leftDice.setAttribute("src", `images/dice${randomNumber1}.png`)
 
 rightDice.setAttribute("src", `images/dice${randomNumber2}.png`)
 
+
+
+let winner = document.querySelector("h1")
+
+//ternary to change heading 
+winner.innerText = randomNumber1 === randomNumber2 ? "Draw" : randomNumber1 > randomNumber2 ? "Player 1 Wins!" : "Player 2 Wins!"
+
+
+//if else to change heading
+
+// if(randomNumber1 === randomNumber2){
+//     winner.innerText = "Tie!"
+// } else if (randomNumber1 > randomNumber2 ){
+//     winner.innerText = "Player 1 Wins!"
+// } else {
+//     winner.innerText = "Player 2 Wins!"
+// }
